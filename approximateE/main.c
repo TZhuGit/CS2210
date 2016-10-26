13153b7b95620c7fed90f634633bee11aa6cdf78
Tony Zhu <T@Tonys-MacBook-Pro.local>
2016-10-25 22:44:27 -0400

    Initial Commit

---
+++ approximateE/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  approximateE
+//
+//  Created by Tony Zhu on 2016-10-25.
+//  Copyright © 2016 Tony Zhu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
